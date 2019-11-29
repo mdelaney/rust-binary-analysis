03d79a6b9f303be5c9a1f9093225d8423ff1e7a1
Matthew Delaney <matthew.l.delaney@gmail.com>
2019-11-29 11:37:04 -0800

    first try with linear disassembly using binload

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,52 @@
+extern crate binload;
 extern crate capstone;
 
 use capstone::prelude::*;
 
-const X86_CODE: &[u8] = b"\x55\x48\x8b\x05\xb8\x13\x00\x00\xe9\x14\x9e\x08\x00\x45\x31\xe4";
+use binload::elf::section::get_section_by_name;
+use binload::load_from_file;
 
-/// Print register names
-fn reg_names<T, I>(cs: &Capstone, regs: T) -> String
-where
-    T: Iterator<Item = I>,
-    I: Into<RegId>,
-{
-    let names: Vec<String> = regs.map(|x| cs.reg_name(x.into()).unwrap()).collect();
-    names.join(", ")
-}
-
-/// Print instruction group names
-fn group_names<T, I>(cs: &Capstone, regs: T) -> String
-where
-    T: Iterator<Item = I>,
-    I: Into<InsnGroupId>,
-{
-    let names: Vec<String> = regs.map(|x| cs.group_name(x.into()).unwrap()).collect();
-    names.join(", ")
+fn get_instruction_string(instruction: &capstone::Insn) -> String {
+    let mut byte_strings: Vec<String> = vec![];
+    for byte in instruction.bytes() {
+        byte_strings.push(format!("{:02x}", byte));
+    }
+    format!(
+        "{:#016x} - {:25} {:8} {:16}",
+        instruction.address(),
+        byte_strings.join(" "),
+        instruction.mnemonic().unwrap(),
+        instruction.op_str().unwrap()
+    )
 }
 
 fn main() {
-    let cs = Capstone::new()
+    // get raw binary
+    let elf = load_from_file("/home/mdelaney/infosec/reverse/crackmes/IOLI/bin-linux/crackme0x00");
+    let text_section = get_section_by_name(".text", &elf.section_headers).unwrap();
+    let text_binary = text_section.get_data(&elf.data);
+
+    // init Capstone
+    let cs: Capstone = Capstone::new()
         .x86()
-        .mode(arch::x86::ArchMode::Mode64)
-        .syntax(arch::x86::ArchSyntax::Att)
+        .mode(arch::x86::ArchMode::Mode32)
+        .syntax(arch::x86::ArchSyntax::Intel)
         .detail(true)
         .build()
         .expect("Failed to create Capstone object");
 
-    let insns = cs
-        .disasm_all(X86_CODE, 0x1000)
+    // do disassembly
+    let instructions = cs
+        .disasm_all(text_binary, text_section.address)
         .expect("Failed to disassemeble");
-    println!("Found {} instructions", insns.len());
-    for i in insns.iter() {
-        println!();
-        println!("{}", i);
 
-        let detail: InsnDetail = cs.insn_detail(&i).expect("Failed to get insn detail");
-        let arch_detail: ArchDetail = detail.arch_detail();
-        let ops = arch_detail.operands();
+    println!("Found {} instructions", instructions.len());
 
-        let output: &[(&str, String)] = &[
-            ("insn id:", format!("{:?}", i.id().0)),
-            ("bytes:", format!("{:?}", i.bytes())),
-            ("read regs:", reg_names(&cs, detail.regs_read())),
-            ("write regs:", reg_names(&cs, detail.regs_write())),
-            ("insn groups:", group_names(&cs, detail.groups())),
-        ];
+    for i in instructions.iter() {
+        println!("{}", get_instruction_string(&i));
 
-        for &(ref name, ref message) in output.iter() {
-            println!("{:4}{:12} {}", "", name, message);
-        }
-
-        println!("{:4}operands: {}", "", ops.len());
-        for op in ops {
-            println!("{:8}{:?}", "", op);
-        }
+        //let detail: InsnDetail = cs.insn_detail(&i).expect("Failed to get insn detail");
+        //let arch_detail: ArchDetail = detail.arch_detail();
+        //let ops = arch_detail.operands();
     }
 }
